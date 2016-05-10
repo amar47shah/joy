ad717eab212a629f7525f97cf2616d97970f7ebd
Owain Lewis <owain@owainlewis.com>
2016-05-10 21:53:00 +0100

    split files

--- src/Language/Joy/Core.hs
+++ src/Language/Joy/Core.hs
@@ -1,1 +1,66 @@
 module Language.Joy.Core where
+
+import           Control.Applicative ((<$>))
+import qualified Data.Map            as M
+
+import           Language.Joy.Parser
+
+-----------------------------------------
+-- | Standard lib
+-----------------------------------------
+
+type JoyF = [Joy] -> Either String [Joy]
+
+add :: JoyF
+add (JoyNumber x: JoyNumber y:xs) = pure $ JoyNumber (x + y) : xs
+add _ = Left "invalid state"
+
+dup :: JoyF
+dup (x:xs) = pure (x:x:xs)
+dup _ = Left "invalid state"
+
+cons (JoyQuote qs : x : xs) = pure $ JoyQuote (x : qs) : xs
+cons _ = Left "invalid state"
+
+-- | Swaps the first two elements on the stack.
+swap :: JoyF
+swap (x:y:xs) = pure $ y:x:xs
+swap _ = Left "invalid state"
+
+-----------------------------------------
+-- | Combinators
+-----------------------------------------
+
+i :: [Joy] -> [Joy]
+i ((JoyQuote x) : xs) = eval x xs
+
+-----------------------------------------
+-- | Prelude env
+-----------------------------------------
+
+prelude :: M.Map [Char] JoyF
+prelude =
+    M.fromList [ ("+", add)
+               , ("dup", dup)
+               , ("swap", swap)
+               , ("cons", cons)
+               ]
+
+-----------------------------------------
+-- | Compiler
+-----------------------------------------
+
+type RuntimeStack = [Joy]
+type ProgramStack = [Joy]
+
+eval :: RuntimeStack -> ProgramStack -> [Joy]
+eval s [] = s
+eval stack (value@(JoyBool _) : xs) = eval (value : stack) xs
+eval stack (value@(JoyNumber _) : xs) = eval (value : stack) xs
+eval stack (value@(JoyQuote _) : xs) = eval (value : stack) xs
+eval stack (value@(JoyLiteral l) : xs) = case (M.lookup l prelude) of
+                                             Just f ->
+                                               case (f stack) of
+                                                 Left e -> error e
+                                                 Right s -> eval s xs
+                                             Nothing -> error "Unbound literal"
--- src/Language/Joy/Parser.hs
+++ src/Language/Joy/Parser.hs
@@ -1,9 +1,9 @@
 module Language.Joy.Parser
-       (
+       ( Joy(..)
+       , parseJoy
        ) where
 
 import           Control.Applicative                ((<$>))
-import qualified Data.Map                           as M
 import           Text.ParserCombinators.Parsec
 import           Text.ParserCombinators.Parsec.Char (letter)
 
@@ -13,6 +13,10 @@ data Joy = JoyNumber Integer
          | JoyQuote [Joy]
          | JoyBool Bool
            deriving ( Show, Eq )
+
+-----------------------------------------
+-- | Parsers
+-----------------------------------------
 
 parseNumber :: Parser Joy
 parseNumber = (JoyNumber . read) <$> many1 digit
@@ -36,67 +40,11 @@ parseLiteral = do
     x <- many1 letter
     return $ JoyLiteral x
 
+-----------------------------------------
+
 parseExpr :: Parser Joy
 parseExpr = parseList <|> parseNumber <|> parseString <|> parseLiteral
 
 parseJoy :: String -> Either ParseError [Joy]
 parseJoy input = parse parser "JOY" input
     where parser = many1 (spaces *> parseExpr <* spaces)
-
--------------------------------
-
-exception = error "Invalid state"
-
------------------------------------------
--- Prelude
------------------------------------------
-
-type JoyF = [Joy] -> [Joy]
-
-add :: JoyF
-add (JoyNumber x: JoyNumber y:xs) = JoyNumber (x + y) : xs
-add _ = exception
-
-dup :: JoyF
-dup (x:xs) = x:x:xs
-dup _ = exception
-
-cons (JoyQuote qs : x : xs) = JoyQuote (x : qs) : xs
-cons _ = exception
-
--- Swaps the first two elements on the stack.
-swap :: JoyF
-swap (x:y:xs) = y:x:xs
-swap _ = exception
-
--- Combinators
-
-i :: [Joy] -> [Joy]
-i ((JoyQuote x) : xs) = eval x xs
-
-prelude :: M.Map [Char] JoyF
-prelude =
-    M.fromList [ ("+", add)
-               , ("dup", dup)
-               , ("swap", swap)
-               , ("cons", cons)
-               ]
------------------------------------------
-
--- Compiler
-
-type RuntimeStack = [Joy]
-type ProgramStack = [Joy]
-
--- | The evaluator for a Joy program
--- The first list is the Joy runtime stack
--- The second list is the Joy input stack
--- The third list is the output of evaluation
-eval :: RuntimeStack -> ProgramStack -> [Joy]
-eval s [] = s
-eval stack (value@(JoyBool _) : xs) = eval (value : stack) xs
-eval stack (value@(JoyNumber _) : xs) = eval (value : stack) xs
-eval stack (value@(JoyQuote _) : xs) = eval (value : stack) xs
-eval stack (value@(JoyLiteral l) : xs) = case (M.lookup l prelude) of
-                                             Just f -> eval (f stack) xs
-                                             Nothing -> error "Unbound literal"
