0d2714bebe9b575542e5d778e200c6223f8c3277
Owain Lewis <owain@owainlewis.com>
2016-05-22 12:45:26 +0100

    nice stateful version

--- src/Language/Joy/Interpreter.hs
+++ src/Language/Joy/Interpreter.hs
@@ -7,6 +7,7 @@ module Language.Joy.Interpreter
 
 import           Control.Exception.Base (Exception, throw)
 import           Control.Monad.State
+import qualified Data.Map               as M
 import           Language.Joy.Parser    (Joy (..))
 
 data InterpeterException =
@@ -16,6 +17,15 @@ data InterpeterException =
 instance Exception InterpeterException
 
 type Stack = [Joy]
+
+data Interpreter = Interpreter {
+    stack :: [Joy]                   -- The runtime stack (current program state)
+  , env   :: M.Map String [Joy]      -- The global environment defined by user
+} deriving ( Show )
+
+-------------------------------------------------
+-- Core stack operations
+-------------------------------------------------
 
 pop ::  StateT Stack IO Joy
 pop = do
@@ -58,9 +68,42 @@ swap = do
          return ()
        _ -> liftIO . throw $ InvalidStateException "Expected two elements on the stack"
 
-program = do
-    push (JoyNumber 10)
-    push (JoyNumber 2)
-    pop
+-------------------------------------------------
+-- Native
+-------------------------------------------------
 
-example = runStateT program []
+dot :: StateT Stack IO ()
+dot = do
+    stack <- get
+    case stack of
+      (x:xs) -> liftIO . print . show $ x
+      [] -> return ()
+
+-- Runs a binary operation on the stack
+binOp :: (Integer -> Integer -> Integer) -> StateT Stack IO ()
+binOp op = do
+    stack <- get
+    case stack of
+      (JoyNumber x : JoyNumber y : xs) -> do
+          put (JoyNumber (x `op` y) : xs)
+          return ()
+      _ -> liftIO . throw $ InvalidStateException "Expected two numbers on the stack"
+
+-------------------------------------------------
+
+-------------------------------------------------
+-- Stateful evaluation
+-------------------------------------------------
+
+eval :: [Joy] -> StateT Stack IO ()
+eval [] = return ()
+eval ((JoyNumber n) : xs) = push (JoyNumber n) >> eval xs
+eval ((JoyLiteral ".") : xs) = dot >> eval xs
+eval ((JoyLiteral "+") : xs) = binOp (+) >> eval xs
+
+-------------------------------------------------
+
+eg = [JoyNumber 1, JoyNumber 2, JoyLiteral "+", JoyLiteral "."]
+
+exec :: [Joy] -> IO ()
+exec program = runStateT (eval program) [] >> return ()
