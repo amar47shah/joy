9abede1a1043d1e5179e9062315377234a24b0a7
Owain Lewis <owain@owainlewis.com>
2016-07-17 20:36:09 +0100

    docs

--- src/Language/Joy/Std.hs
+++ src/Language/Joy/Std.hs
@@ -21,9 +21,12 @@ import Language.Joy.State
 import Data.Map as M
 import Data.Monoid((<>))
 
+-- | The following table of operations represent the primary
+-- | constructs that form the Joy programming language.
 allOperations :: Map [Char] (State -> JoyResult)
 allOperations = fromList[ ("dup", dup)
                         , ("+", plus)
+                        , ("-", plus)
                         , ("i", i)
                         , (".", dot)
                         ]
@@ -42,24 +45,35 @@ succeedWith = pure . Right
 failFor :: Monad m => String -> m (Either JoyError b)
 failFor op = failWith $ InvalidState ("Invalid state for operation " <> op)
 
--- | Is there a more elegant way to bind these and prevent terminal recursion?
-
+-- | Basic addition
 plus :: State -> JoyResult
 plus (State (JoySymbol("+"):ys) ((JoyNumber x):(JoyNumber y):xs) env) =
     succeedWith $ State ys (JoyNumber(x+y):xs) env
 plus _ = failFor "+"
 
+-- | Basic subtraction
+minus :: State -> JoyResult
+minus (State (JoySymbol("-"):ys) ((JoyNumber x):(JoyNumber y):xs) env) =
+    succeedWith $ State ys (JoyNumber(x-y):xs) env
+minus _ = failFor "-"
+
+-- | Duplicates the item on the top of the stack
 dup :: State -> JoyResult
 dup (State (JoySymbol("dup"):ys) (x:xs) env) = succeedWith $ State ys (x:x:xs) env
 dup _ = failFor "dup"
 
+-- | Dot is used to print the current state of the run stack
 dot :: State -> JoyResult
 dot state@(State (JoySymbol("."):ys) output env) = do
     print (show state)
     return . pure $ State ys output env
 dot _ = failFor "."
 
--- | Put all values inside the quoted list onto the stack
+-- | These combinators are special in that they dequote stack items
+-- | The "i" combinator simply executes the top item on the stack.
+-- | @
+-- | [A] i == A
+-- | @
 i :: State -> JoyResult
 i state@(State (JoySymbol("i"):ys) (JoyQuote vs:xs) env) = succeedWith $ State (vs++ys) xs env
 i _ = failFor "i"
